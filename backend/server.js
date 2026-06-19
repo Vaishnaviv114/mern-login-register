c79abde697c527f843ce1228b81bbdce41730a2b
Vaishnaviv114 <vaishnaviverma3452@gmail.com>
2026-06-20 03:09:36 +0530

    Add CORS support

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import authRoutes from './routes/auth.js';
 import { connectDB } from './config/db.js';
+import cors from "cors";
 
 
 
@@ -9,6 +10,9 @@ dotenv.config();
 const PORT = process.env.PORT || 5000;
 
 const app = express();
+app.use(cors({
+  origin: "https://mern-login-register-frontend.onrender.com"
+}));
 
 app.use(express.json());
 
